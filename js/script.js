bbb6fb86da6f8c1fbf81c615256fe3da83826c21
Leanne Hagger <leannehagger@gmail.com>
2018-11-21 10:19:31 +0000

    added js file and started adding classes to buttons

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var choices = ["3GB", "6GB", "15GB", "30GB", "monthly", "annually"];
+
+var price = 0;
+
+$(".wolf--price").click(function(event) {
+var wolf_gb = this.getAttribute("data-gb");
+var wolf_price = this.getAttribute("data-price");
+console.log(wolf_price);
+});
+
+wolf_price = parseInt(this.getAttribute("data-price"));
+
+function monthly() {
+var wolf_price =
+
+
+}
+
+
+
+  function pay--annually() {
+  var wolf_price = Math.floor(
+  }
+// }
+// // wolf_price = parseInt(this.getAttribute("data-price"));
