1154529cc5ce50b2e3eeaa3da5820207cb30503a
Leanne Hagger <leannehagger@gmail.com>
2018-11-21 11:55:29 +0000

    altering chatterbox calculator adding css to nav

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var choices = ["3GB", "6GB", "15GB", "30GB", "monthly", "annually"];
 
 var price = 0;
 
+// wolf price calculations and price display//
+
 $(".wolf--price").click(function(event) {
   var wolf_gb = this.getAttribute("data-gb");
   var wolf_price = this.getAttribute("data-price");
@@ -17,4 +19,28 @@ $(".annually").click(function(event) {
   $("#price").text(price * 12);
 });
 
-wolf_price = parseInt(this.getAttribute("data-price"));
+// chatterbox price calculations and price display//
+
+$(".chatterbox--price").click(function(event) {
+  var chatterbox_gb = this.getAttribute("data-gb");
+  var chatterbox_price = this.getAttribute("data-price");
+  var chatterbox_mins = this.getAttribute("mins-price");
+  price = chatterbox_price;
+  console.log(chatterbox_price);
+});
+
+$(".mins-price").click(function(event) {
+  $("#price").text(price);
+});
+
+$(".monthly").click(function(event) {
+  $("#price").text(price);
+});
+
+$(".annually").click(function(event) {
+  $("#price").text(price * 12);
+});
+
+// wolf_price = parseInt(this.getAttribute("data-price"));
+chatterbox_price = parseInt(this.getAttribute("data-price"));
+chatterbox_price = parseInt(this.getAttribute("mins-price"));
