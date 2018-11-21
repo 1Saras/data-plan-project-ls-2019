d2b6705de247a4b2cd29d44ad67df6c6055209e7
Leanne Hagger <leannehagger@gmail.com>
2018-11-21 11:22:22 +0000

    updated button classes in chatterbox html and beautified this doc as well as js doc and lone wolf html

--- js/script.js
+++ js/script.js
@@ -3,18 +3,18 @@ var choices = ["3GB", "6GB", "15GB", "30GB", "monthly", "annually"];
 var price = 0;
 
 $(".wolf--price").click(function(event) {
-var wolf_gb = this.getAttribute("data-gb");
-var wolf_price = this.getAttribute("data-price");
-price = wolf_price;
-console.log(wolf_price);
+  var wolf_gb = this.getAttribute("data-gb");
+  var wolf_price = this.getAttribute("data-price");
+  price = wolf_price;
+  console.log(wolf_price);
 });
 
 $(".monthly").click(function(event) {
-$("#price").text(price);
+  $("#price").text(price);
 });
 
 $(".annually").click(function(event) {
-$("#price").text(price*12);
+  $("#price").text(price * 12);
 });
 
 wolf_price = parseInt(this.getAttribute("data-price"));
