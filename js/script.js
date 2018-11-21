bc0a8c627117c16d92f3add504b7227f11402efd
Leanne Hagger <leannehagger@gmail.com>
2018-11-21 11:04:06 +0000

    got lone wolf calculators to work!

--- js/script.js
+++ js/script.js
@@ -5,21 +5,16 @@ var price = 0;
 $(".wolf--price").click(function(event) {
 var wolf_gb = this.getAttribute("data-gb");
 var wolf_price = this.getAttribute("data-price");
+price = wolf_price;
 console.log(wolf_price);
 });
 
+$(".monthly").click(function(event) {
+$("#price").text(price);
+});
+
+$(".annually").click(function(event) {
+$("#price").text(price*12);
+});
+
 wolf_price = parseInt(this.getAttribute("data-price"));
-
-function monthly() {
-var wolf_price =
-
-
-}
-
-
-
-  function pay--annually() {
-  var wolf_price = Math.floor(
-  }
-// }
-// // wolf_price = parseInt(this.getAttribute("data-price"));
